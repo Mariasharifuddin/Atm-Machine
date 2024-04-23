65ab58ab56bae8a1194743617ef9012dad268455
Mariasharifuddin <161883333+Mariasharifuddin@users.noreply.github.com>
2024-04-23 23:16:25 +0500

    push my calculator to github

---
+++ main.ts
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+// Initialize user balance and pin code
+let myBalance = 5000; // Dollar
+
+let myPin = 1234;
+   // print welcome message
+console.log("Welcome to code with mariya - Atm Machine");
+
+
+let PinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your pin code:"
+    }
+])
+if (PinAnswer.pin === myPin){
+    console.log("pin is correct, login Sucessfully!");
+     console.log(`Current Account Balance is ${myBalance}`)
+
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "Select an operation:",
+            choices: ["WithDraw Amount", "Check Balance"]
+        }
+    ])
+
+    if(operationAns.operation === "WithDraw Amount"){
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "number",
+                message: "Enter the amount to withdraw:"
+            }
+        ])
+        if(amountAns.amount > myBalance){
+            console.log("Insufficient Balance");
+        }
+        else{
+            myBalance -= amountAns.amount;
+            console.log(`${amountAns.amount} withdraw sucessfully`);
+            console.log(`your Remaining Balance is: ${myBalance}`);
+        }
+    } 
+    else if(operationAns.operation === "Check Balance"){
+        console.log(`Your Account Balance is: ${myBalance}`);
+    } 
+}
+else{
+    console.log("pin is Incorrect, Try Again!" );
+}
